d7d1de7258a489f169ad25c842afdaa342a3306c
Md. Enzamamul Haque Shiplu <75667617+shiplu2111@users.noreply.github.com>
2024-03-18 15:08:15 +0600

    Initial commit

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth:api');
+    }
+    public function index()
+    {
+        return response()->json([
+            'message' => 'Hello World!',
+            'user' => auth()->user()
+        ]);
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\TestController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+});
+
+
+Route::middleware(['api'])->group(function () {
+    Route::get('/test', [TestController::class, 'index']);
+
+
+});
